b43bfb4cc5f79610bf44ada94464b5d48c9be0b0
Injamuri Krutika <kruteeka.injamuri@gmail.com>
2019-03-18 19:52:44 +0530

    Add upload-documents route

--- Project/views/src/app/app-routing.module.ts
+++ Project/views/src/app/app-routing.module.ts
@@ -1,20 +1,20 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { BasicComponent } from './basic/basic.component';
-import { NewUserComponent } from './new-user/new-user.component';
-import { IncomeEmiComponent } from './income-emi/income-emi.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { BasicComponent } from "./basic/basic.component";
+import { NewUserComponent } from "./new-user/new-user.component";
+import { IncomeEmiComponent } from "./income-emi/income-emi.component";
+import { UploadDocumentsComponent } from "./upload-documents/upload-documents.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: '/basic', pathMatch: 'full' },
-  { path: 'basic', component: BasicComponent },
-  { path: 'new-user', component: NewUserComponent },
-  { path: 'income-emi', component: IncomeEmiComponent }
-
-
+  { path: "", redirectTo: "/basic", pathMatch: "full" },
+  { path: "basic", component: BasicComponent },
+  { path: "new-user", component: NewUserComponent },
+  { path: "income-emi", component: IncomeEmiComponent },
+  { path: "upload-documents", component: UploadDocumentsComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
