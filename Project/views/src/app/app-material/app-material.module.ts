ca4e34149d5ec1446c0c2f293141124c8f887ee9
Injamuri Krutika <kruteeka.injamuri@gmail.com>
2019-03-18 19:52:11 +0530

    Add Material modules to use tags

--- Project/views/src/app/app-material/app-material.module.ts
+++ Project/views/src/app/app-material/app-material.module.ts
@@ -1,9 +1,46 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { MatInputModule, MatButtonModule, MatCardModule,MatSelectModule,MatOptionModule } from "@angular/material";
+import {
+  MatInputModule,
+  MatButtonModule,
+  MatCardModule,
+  MatSelectModule,
+  MatOptionModule,
+  MatStepperModule,
+  MatIconModule,
+  MatDialogModule,
+  MatRadioModule,
+  MatGridListModule,
+  MatTableModule
+} from "@angular/material";
 
 @NgModule({
-  imports: [CommonModule, MatInputModule, MatButtonModule, MatCardModule,MatSelectModule,MatOptionModule],
-  exports: [MatInputModule, MatButtonModule, MatCardModule,MatSelectModule,MatOptionModule]
+  imports: [
+    CommonModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatStepperModule,
+    MatIconModule,
+    MatDialogModule,
+    MatRadioModule,
+    MatGridListModule,
+    MatTableModule
+  ],
+  exports: [
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatStepperModule,
+    MatIconModule,
+    MatDialogModule,
+    MatRadioModule,
+    MatGridListModule,
+    MatTableModule
+  ]
 })
 export class AppMaterialModule {}
