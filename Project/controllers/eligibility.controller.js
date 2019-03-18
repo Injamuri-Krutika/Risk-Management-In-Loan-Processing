84538a9d0f19230150d5097d8052a5db1d81aef9
Injamuri Krutika <kruteeka.injamuri@gmail.com>
2019-03-18 19:47:31 +0530

    Add parameters to the get URL

--- Project/controllers/eligibility.controller.js
+++ Project/controllers/eligibility.controller.js
@@ -2,23 +2,29 @@ const Eligibility = require("../models/eligibility.model");
 
 //get
 exports.eligibilityDetails = function(req, res) {
-  Eligibility.find({ eligibilityId: req.params.id }).exec(
-    (err, eligibility) => {
-      if (err) return next(err);
-      res.send(eligibility);
-    }
-  );
+  age = parseInt(req.params.age);
+  id = parseInt(req.params.id);
+  Eligibility.find({ eligibilityId: id }).exec((err, eligibility) => {
+    if (err) return next(err);
+    record = eligibility[0];
+    if (record)
+      if (record.minAge <= age && age <= record.maxAge) res.send(record);
+      else res.send({ isInEligible: true });
+    else res.send("Record not found.");
+  });
 };
 
 //create
-exports.eligibilityCreate = function(req, res) {
+exports.eligibilityCreate = function(req, res, next) {
   console.log("Inside create logic");
   let eligibility = new Eligibility({
     eligibilityId: req.body.eligibilityId,
     minAge: req.body.minAge,
     maxAge: req.body.maxAge,
     minSalary: req.body.minSalary,
-    maxSalary: req.body.maxSalary
+    minExperience: req.body.minExperience,
+    minCurrentCompanyExperience: req.body.minCurrentCompanyExperience,
+    netTakeHome: req.body.netTakeHome
   });
 
   eligibility.save(function(err) {
@@ -30,3 +36,16 @@ exports.eligibilityCreate = function(req, res) {
 };
 
 //update
+//get
+exports.eligibilityDelete = function(req, res, next) {
+  Eligibility.deleteMany({ eligibilityId: req.params.id }).exec(err => {
+    if (err) return next(err);
+    // console.log(typeof req.params.id);
+    // record = eligibility[0];
+    // age = parseInt(req.params.age);
+    // console.log(record.minAge, age, record.maxAge);
+    res.send("Deleted Successfully!");
+    // if (record.minAge < age && age < record.maxAge) res.send(record);
+    // else res.send("Ineligible");
+  });
+};
