b3cfd0b72822387e86a2ddda7f5f30ebf5666f42
Injamuri Krutika <kruteeka.injamuri@gmail.com>
2019-04-07 09:26:00 +0530

    Add logic to display the personal and loan details

--- Project/views/src/app/customers-dashboard/customers-dashboard.component.ts
+++ Project/views/src/app/customers-dashboard/customers-dashboard.component.ts
@@ -1,33 +1,37 @@
-import { Component } from '@angular/core';
-import { map } from 'rxjs/operators';
-import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { Component, OnInit } from "@angular/core";
+import { map } from "rxjs/operators";
+import { Breakpoints, BreakpointObserver } from "@angular/cdk/layout";
+import { Customer } from "../Classes";
 
 @Component({
-  selector: 'app-customers-dashboard',
-  templateUrl: './customers-dashboard.component.html',
-  styleUrls: ['./customers-dashboard.component.css']
+  selector: "app-customers-dashboard",
+  templateUrl: "./customers-dashboard.component.html",
+  styleUrls: ["./customers-dashboard.component.css"]
 })
-export class CustomersDashboardComponent {
+export class CustomersDashboardComponent implements OnInit {
   /** Based on the screen size, switch from standard to one column per row */
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({ matches }) => {
       if (matches) {
         return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
+          { title: "Customer Details", cols: 1, rows: 1 },
+          { title: "Loan Details", cols: 1, rows: 1 }
         ];
       }
 
       return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
+        { title: "Customer Details", cols: 2, rows: 1 },
+        { title: "Loan Details", cols: 2, rows: 1 }
       ];
     })
   );
 
   constructor(private breakpointObserver: BreakpointObserver) {}
+  customerDetails: Customer;
+  loan: Customer[];
+  ngOnInit() {
+    this.customerDetails = JSON.parse(sessionStorage.getItem("userDetails"));
+    this.loan = [this.customerDetails];
+    console.log(this.customerDetails);
+  }
 }
