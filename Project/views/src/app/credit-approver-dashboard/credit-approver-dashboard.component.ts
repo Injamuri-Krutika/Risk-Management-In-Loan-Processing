2b1f3e0467b944545e68898d628a32fea14f12e0
Injamuri Krutika <kruteeka.injamuri@gmail.com>
2019-04-09 05:06:11 +0530

    Credit Approver fuctionality

--- Project/views/src/app/credit-approver-dashboard/credit-approver-dashboard.component.ts
+++ Project/views/src/app/credit-approver-dashboard/credit-approver-dashboard.component.ts
@@ -1,33 +1,80 @@
-import { Component } from '@angular/core';
-import { map } from 'rxjs/operators';
-import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { Component, OnInit } from "@angular/core";
+import { map } from "rxjs/operators";
+import { Breakpoints, BreakpointObserver } from "@angular/cdk/layout";
+import { CreditApprover, Customer } from "../Classes";
+import { LoanDetailsService } from "../loan-details.service";
 
 @Component({
-  selector: 'app-credit-approver-dashboard',
-  templateUrl: './credit-approver-dashboard.component.html',
-  styleUrls: ['./credit-approver-dashboard.component.css']
+  selector: "app-credit-approver-dashboard",
+  templateUrl: "./credit-approver-dashboard.component.html",
+  styleUrls: ["./credit-approver-dashboard.component.css"]
 })
-export class CreditApproverDashboardComponent {
+export class CreditApproverDashboardComponent implements OnInit {
   /** Based on the screen size, switch from standard to one column per row */
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({ matches }) => {
       if (matches) {
         return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
+          { title: "Card 1", cols: 1, rows: 1 },
+          { title: "Card 2", cols: 1, rows: 1 },
+          { title: "Card 3", cols: 1, rows: 1 },
+          { title: "Card 4", cols: 1, rows: 1 }
         ];
       }
 
       return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
+        { title: "Card 1", cols: 2, rows: 1 },
+        { title: "Card 2", cols: 1, rows: 1 },
+        { title: "Card 3", cols: 1, rows: 2 },
+        { title: "Card 4", cols: 1, rows: 1 }
       ];
     })
   );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private loanDetails: LoanDetailsService
+  ) {}
+  panelOpenState: boolean = true;
+  creditApprover: CreditApprover;
+  appliedLoans: Customer[];
+  displayedColumns: string[];
+  viewDetails: Customer;
+  ngOnInit() {
+    this.creditApprover = JSON.parse(sessionStorage.getItem("creditApprover"));
+    this.loanDetails.getAppliedLoans().subscribe(res => {
+      this.appliedLoans = res.loans;
+    });
+    this.displayedColumns = [
+      "requestId",
+      "requestDate",
+      "firstName",
+      "lastName",
+      "finalLoanAmount",
+      "tenure",
+      "finalEMI"
+    ];
+  }
+  verifyDetails(index) {
+    this.panelOpenState = false;
+    this.viewDetails = this.appliedLoans[index];
+  }
+
+  updateLoan(status, requestId, email) {
+    var requestObject = {
+      loanStatus: status,
+      requestId,
+      email
+    };
+    this.loanDetails.updateAppliedLoan(requestObject).subscribe(res => {
+      console.log(res);
+      if (res.nModified == 1 && res.n == 1) {
+        this.loanDetails.getAppliedLoans().subscribe(res => {
+          this.appliedLoans = res.loans;
+          this.viewDetails = null;
+          this.panelOpenState = true;
+        });
+      }
+    });
+  }
 }
--- Project/views/src/app/login/login.component.ts
+++ Project/views/src/app/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component } from "@angular/core";
 import { Router, ActivatedRoute } from "@angular/router";
 import { FormGroup, FormControl, Validators } from "@angular/forms";
 import { LoginService } from "../login.service";
+import { CreditApprover, AppliedLoans } from "../Classes";
+import { LoanDetailsService } from "../loan-details.service";
 
 @Component({
   selector: "app-login",
@@ -23,12 +25,25 @@ export class LoginComponent {
     });
   }
 
+  creditApprover: CreditApprover;
+  appliedloans: AppliedLoans;
   onSubmit() {
     // TODO: Use EventEmitter with form value
     console.log(this.profileForm.value);
 
     if (this.profileForm.get("user").value.includes("Credit")) {
+      this.creditApprover = new CreditApprover();
+      this.creditApprover.firstName = "Credit";
+      this.creditApprover.lastName = "Approver";
+      this.creditApprover.dob = "1986-12-12";
+      this.creditApprover.email = "creditapprover@gmail.com";
+      this.creditApprover.phoneNumber = 7331121101;
+      sessionStorage.setItem(
+        "creditApprover",
+        JSON.stringify(this.creditApprover)
+      );
       this.router.navigate(["/credit-approver-dashboard"]);
+
       return;
     }
     this.loginService
